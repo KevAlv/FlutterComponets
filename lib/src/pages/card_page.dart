cf131da5e68ad145e9b0309ad4c81bb65ca09ccf
Kevin <Aok4001032@gmail.com>
2019-12-16 12:08:52 -0400

    image y fadeimage

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -6,19 +6,25 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Cards"),),
+      appBar: AppBar(
+        title: Text("Cards"),
+      ),
       body: _listView(),
-          );
-        }
-      
-        _listView() {
-          return ListView(
-            padding: EdgeInsets.all(10.0),
-            children: <Widget>[
-              _cardTipo1()
-            ],
-          );
-        }
+    );
+  }
+
+  _listView() {
+    return ListView(
+      padding: EdgeInsets.all(10.0),
+      children: <Widget>[
+        _cardTipo1(),
+        SizedBox(
+          height: 30,
+        ),
+        _cardTipo2()
+      ],
+    );
+  }
 
   _cardTipo1() {
     return Card(
@@ -27,21 +33,41 @@ class CardPage extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.photo_album),
             title: Text("Soy El Tiutlo"),
-            subtitle: Text("Soy el subtitulo que ustedes veran en la pantallas"),
+            subtitle:
+                Text("Soy el subtitulo que ustedes veran en la pantallas"),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               FlatButton(
                 child: Text("Cancelar"),
-                onPressed: (){},
+                onPressed: () {},
               ),
               FlatButton(
                 child: Text("Ok"),
-                onPressed: (){},
+                onPressed: () {},
               ),
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+              image: NetworkImage('https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM-970-80.jpg') ,
+              placeholder:AssetImage('assets/jar-loading.gif') ,
+              fadeInDuration:Duration(milliseconds:200),
+              height: 300.0,
+              fit:BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text("no se"))
         ],
       ),
     );
