d53014840e0877f915f89ac5f285ebd927a650f6
Kevin <Aok4001032@gmail.com>
2019-12-16 08:31:59 -0400

    cargue el archivo Json

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,25 +7,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(title: Text("Componentes"),),
-     body: _lista(),
-         );
-       }
-     
-      Widget _lista() {
-        return ListView(
-          children: listaItems(),
-        );
-      }
+      appBar: AppBar(
+        title: Text("Componentes"),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    print(MenuProvider.opciones);
+    return ListView(
+      children: listaItems(),
+    );
+  }
 
   List<Widget> listaItems() {
     return [
       ListTile(
-      title: Text("data"),
-    ),
-    ListTile(
-      title: Text("data"),
-    )
+        title: Text("data"),
+      ),
+      ListTile(
+        title: Text("data"),
+      )
     ];
   }
 }
